ac264d31d1773835832bb19ab15a40898d54e8cb
Tatsuya <tatsu32ya@gmail.com>
2023-08-15 15:19:51 +0900

    feat コーディング

---
+++ src/tufspot/resources/views/components/template.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="image/icons8-pencil-94.png">
+
+    <title>{{ $title }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+
+    {{-- <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --> --}}
+    <link href="{{ asset('css/style_uru.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    {{-- <script src="{{ asset('js/app.js') }}" async></script>
+    <script src="{{ asset('js/uru_top.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('js/jquery-3.5.1.min.js') }}" defer></script> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2.3.0/dist/flickity.css" media="screen">
+</head>
+
+<body alink=”#0056b3 class="body">
+    <script src="https://unpkg.com/flickity@2.3.0/dist/flickity.pkgd.min.js"></script>
+    <div class="wrapper">
+        {{-- slotはx-templateの中でx-breadや他のcomponents読み込む用 --}}
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
--- src/tufspot/routes/web.php
+++ src/tufspot/routes/web.php
@@ -11,6 +11,49 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/top', function () {
+    return view('index');
+});
+Route::get('/feature_article', function () {
+    return view('feature_article');
+});
+Route::get('/feature_list', function () {
+    return view('feature_list');
+});
+// 中身同じ
+Route::get('/search_result', function () {
+    return view('search_result');
+});
+Route::get('/hashtag_result', function () {
+    return view('hashtag_result');
+});
+// mypage
+Route::get('/mypage', function () {
+    return view('mypage');
+});
+// ライター一覧
+Route::get('/writer', function () {
+    return view('writer_list');
+});
+// ライター詳細
+Route::get('/writer/detail', function () {
+    return view('writer_detail');
+});
+// 記事詳細
+Route::get('/article/1', function () {
+    return view('article_detail');
+});
+// TUFSPOTについて
+Route::get('/about', function () {
+    return view('about');
+});
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
